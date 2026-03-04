2a29ef3533d9963546817fc2f51aaa01692bea77
Antoine Cyr Bouchard <antoine.cyr-bouchard.1@ulaval.ca>
2026-03-04 10:10:18 -0500

    modified psychopy to add check_up

--- bayesian_san_lastrun.py
+++ bayesian_san_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on March 03, 2026, at 13:36
+    on mars 04, 2026, at 10:07
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -33,6 +33,8 @@ import sys  # to get file system encoding
 import psychopy.iohub as io
 from psychopy.hardware import keyboard
 
+# Run 'Before Experiment' code from set_up_2
+import random 
 # Run 'Before Experiment' code from boxes_creation
 from psychopy import visual, core
 
@@ -128,7 +130,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version='',
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='C:\\Users\\labmp\\Desktop\\Sanounou\\bayesian_san_lastrun.py',
+        originPath='C:\\Users\\cbant\\Desktop\\git\\Pain-Bayes\\bayesian_san_lastrun.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
@@ -267,6 +269,12 @@ def setupDevices(expInfo, thisExp, win):
         instr_leave = deviceManager.addDevice(
             deviceClass='keyboard',
             deviceName='instr_leave',
+        )
+    if deviceManager.getDevice('check_up_resp') is None:
+        # initialise check_up_resp
+        check_up_resp = deviceManager.addDevice(
+            deviceClass='keyboard',
+            deviceName='check_up_resp',
         )
     if deviceManager.getDevice('slider_pain_resp') is None:
         # initialise slider_pain_resp
@@ -408,18 +416,14 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
     
     # --- Initialize components for Routine "fix" ---
     fixation_cross = visual.TextStim(win=win, name='fixation_cross',
-        text='+',
+        text='',
         font='Arial',
         pos=(0, 0), draggable=False, height=0.15, wrapWidth=None, ori=0.0, 
         color='white', colorSpace='rgb', opacity=None, 
         languageStyle='LTR',
-        depth=0.0);
+        depth=-1.0);
     
     # --- Initialize components for Routine "vas_indicator" ---
-    # Run 'Begin Experiment' code from pain_indicator_slider
-    
-    
-    
     slider_indicator = visual.Slider(win=win, name='slider_indicator',
         startValue=None, size=(1.0, 0.1), pos=(0, 0), units=win.units,
         labels=['Aucune\ndouleur', 'Pire douleur\nimaginable'], ticks=[0, 100], granularity=0.0,
@@ -427,6 +431,30 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         labelColor='White', markerColor='Red', lineColor='White', colorSpace='rgb',
         font='Arial', labelHeight=0.04,
         flip=False, ori=0.0, depth=-1, readOnly=False)
+    
+    # --- Initialize components for Routine "check_up_routine" ---
+    check_up_text = visual.TextStim(win=win, name='check_up_text',
+        text='TEXTE SUR NIVEAU DE CONFIANCE\n\nn - m pour dire\n\nn = high confidence\n\nm = low confidence',
+        font='Arial',
+        pos=(0, 0), draggable=False, height=0.05, wrapWidth=None, ori=0.0, 
+        color='white', colorSpace='rgb', opacity=None, 
+        languageStyle='LTR',
+        depth=-1.0);
+    check_up_resp = keyboard.Keyboard(deviceName='check_up_resp')
+    high_confidence = visual.TextStim(win=win, name='high_confidence',
+        text='',
+        font='Arial',
+        pos=(1, 0), draggable=False, height=0.05, wrapWidth=None, ori=0.0, 
+        color='white', colorSpace='rgb', opacity=None, 
+        languageStyle='LTR',
+        depth=-3.0);
+    low_confidence = visual.TextStim(win=win, name='low_confidence',
+        text='',
+        font='Arial',
+        pos=(-1, 0), draggable=False, height=0.05, wrapWidth=None, ori=0.0, 
+        color='white', colorSpace='rgb', opacity=None, 
+        languageStyle='LTR',
+        depth=-4.0);
     
     # --- Initialize components for Routine "pain_fix" ---
     fake_pain = visual.TextStim(win=win, name='fake_pain',
@@ -762,6 +790,9 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         fix.status = NOT_STARTED
         continueRoutine = True
         # update component parameters for each repeat
+        # Run 'Begin Routine' code from fix_code_timer
+        length_fix_timer = random.randint(2, 3)
+        fixation_cross.setText('+')
         # store start times for fix
         fix.tStartRefresh = win.getFutureFlipTime(clock=globalClock)
         fix.tStart = globalClock.getTime(format='float')
@@ -787,7 +818,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         if isinstance(conditions_loop, data.TrialHandler2) and thisConditions_loop.thisN != conditions_loop.thisTrial.thisN:
             continueRoutine = False
         fix.forceEnded = routineForceEnded = not continueRoutine
-        while continueRoutine and routineTimer.getTime() < 2.0:
+        while continueRoutine:
             # get current time
             t = routineTimer.getTime()
             tThisFlip = win.getFutureFlipTime(clock=routineTimer)
@@ -818,7 +849,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
             # if fixation_cross is stopping this frame...
             if fixation_cross.status == STARTED:
                 # is it time to stop? (based on global clock, using actual start)
-                if tThisFlipGlobal > fixation_cross.tStartRefresh + 2-frameTolerance:
+                if tThisFlipGlobal > fixation_cross.tStartRefresh + length_fix_timer-frameTolerance:
                     # keep track of stop time/frame for later
                     fixation_cross.tStop = t  # not accounting for scr refresh
                     fixation_cross.tStopRefresh = tThisFlipGlobal  # on global time
@@ -868,13 +899,10 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         fix.tStop = globalClock.getTime(format='float')
         fix.tStopRefresh = tThisFlipGlobal
         thisExp.addData('fix.stopped', fix.tStop)
-        # using non-slip timing so subtract the expected duration of this Routine (unless ended on request)
-        if fix.maxDurationReached:
-            routineTimer.addTime(-fix.maxDuration)
-        elif fix.forceEnded:
-            routineTimer.reset()
-        else:
-            routineTimer.addTime(-2.000000)
+        # Run 'End Routine' code from fix_code_timer
+        thisExp.addData('length_fix_timer', length_fix_timer)
+        # the Routine "fix" was not non-slip safe, so reset the non-slip timer
+        routineTimer.reset()
         
         # --- Prepare to start Routine "vas_indicator" ---
         # create an object to store info about Routine vas_indicator
@@ -1061,6 +1089,234 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         conditions_loop.addData('slider_indicator.response', slider_indicator.getRating())
         conditions_loop.addData('slider_indicator.rt', slider_indicator.getRT())
         # the Routine "vas_indicator" was not non-slip safe, so reset the non-slip timer
+        routineTimer.reset()
+        
+        # --- Prepare to start Routine "check_up_routine" ---
+        # create an object to store info about Routine check_up_routine
+        check_up_routine = data.Routine(
+            name='check_up_routine',
+            components=[check_up_text, check_up_resp, high_confidence, low_confidence],
+        )
+        check_up_routine.status = NOT_STARTED
+        continueRoutine = True
+        # update component parameters for each repeat
+        # Run 'Begin Routine' code from check_up_code
+        if check_up == 'no':
+            continueRoutine = False
+        
+        answer_keys = ['n','m']
+        
+        high_key = answer_keys[0]
+        low_key = answer_keys[1]
+        
+        
+        if expect_uncertainty == 'low':
+            certainty_expectation_answer = low_key
+        elif expect_uncertainty == 'high' :
+            certainty_expectation_answer = high_key
+            
+            
+            
+        
+        
+        
+        # create starting attributes for check_up_resp
+        check_up_resp.keys = []
+        check_up_resp.rt = []
+        _check_up_resp_allKeys = []
+        # allowedKeys looks like a variable, so make sure it exists locally
+        if 'answer_keys' in globals():
+            answer_keys = globals()['answer_keys']
+        high_confidence.setText('Haute confiance')
+        low_confidence.setText('Low confidence')
+        # store start times for check_up_routine
+        check_up_routine.tStartRefresh = win.getFutureFlipTime(clock=globalClock)
+        check_up_routine.tStart = globalClock.getTime(format='float')
+        check_up_routine.status = STARTED
+        thisExp.addData('check_up_routine.started', check_up_routine.tStart)
+        check_up_routine.maxDuration = None
+        # keep track of which components have finished
+        check_up_routineComponents = check_up_routine.components
+        for thisComponent in check_up_routine.components:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        frameN = -1
+        
+        # --- Run Routine "check_up_routine" ---
+        # if trial has changed, end Routine now
+        if isinstance(conditions_loop, data.TrialHandler2) and thisConditions_loop.thisN != conditions_loop.thisTrial.thisN:
+            continueRoutine = False
+        check_up_routine.forceEnded = routineForceEnded = not continueRoutine
+        while continueRoutine:
+            # get current time
+            t = routineTimer.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            
+            # *check_up_text* updates
+            
+            # if check_up_text is starting this frame...
+            if check_up_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                check_up_text.frameNStart = frameN  # exact frame index
+                check_up_text.tStart = t  # local t and not account for scr refresh
+                check_up_text.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(check_up_text, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'check_up_text.started')
+                # update status
+                check_up_text.status = STARTED
+                check_up_text.setAutoDraw(True)
+            
+            # if check_up_text is active this frame...
+            if check_up_text.status == STARTED:
+                # update params
+                pass
+            
+            # *check_up_resp* updates
+            waitOnFlip = False
+            
+            # if check_up_resp is starting this frame...
+            if check_up_resp.status == NOT_STARTED and tThisFlip >= 0-frameTolerance:
+                # keep track of start time/frame for later
+                check_up_resp.frameNStart = frameN  # exact frame index
+                check_up_resp.tStart = t  # local t and not account for scr refresh
+                check_up_resp.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(check_up_resp, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'check_up_resp.started')
+                # update status
+                check_up_resp.status = STARTED
+                # allowed keys looks like a variable named `answer_keys`
+                if not type(answer_keys) in [list, tuple, np.ndarray]:
+                    if not isinstance(answer_keys, str):
+                        answer_keys = str(answer_keys)
+                    elif not ',' in answer_keys:
+                        answer_keys = (answer_keys,)
+                    else:
+                        answer_keys = eval(answer_keys)
+                # keyboard checking is just starting
+                waitOnFlip = True
+                win.callOnFlip(check_up_resp.clock.reset)  # t=0 on next screen flip
+                win.callOnFlip(check_up_resp.clearEvents, eventType='keyboard')  # clear events on next screen flip
+            if check_up_resp.status == STARTED and not waitOnFlip:
+                theseKeys = check_up_resp.getKeys(keyList=list(answer_keys), ignoreKeys=["escape"], waitRelease=False)
+                _check_up_resp_allKeys.extend(theseKeys)
+                if len(_check_up_resp_allKeys):
+                    check_up_resp.keys = _check_up_resp_allKeys[0].name  # just the first key pressed
+                    check_up_resp.rt = _check_up_resp_allKeys[0].rt
+                    check_up_resp.duration = _check_up_resp_allKeys[0].duration
+                    # was this correct?
+                    if (check_up_resp.keys == str(certainty_expectation_answer)) or (check_up_resp.keys == certainty_expectation_answer):
+                        check_up_resp.corr = 1
+                    else:
+                        check_up_resp.corr = 0
+                    # a response ends the routine
+                    continueRoutine = False
+            
+            # *high_confidence* updates
+            
+            # if high_confidence is starting this frame...
+            if high_confidence.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                high_confidence.frameNStart = frameN  # exact frame index
+                high_confidence.tStart = t  # local t and not account for scr refresh
+                high_confidence.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(high_confidence, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'high_confidence.started')
+                # update status
+                high_confidence.status = STARTED
+                high_confidence.setAutoDraw(True)
+            
+            # if high_confidence is active this frame...
+            if high_confidence.status == STARTED:
+                # update params
+                pass
+            
+            # *low_confidence* updates
+            
+            # if low_confidence is starting this frame...
+            if low_confidence.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                low_confidence.frameNStart = frameN  # exact frame index
+                low_confidence.tStart = t  # local t and not account for scr refresh
+                low_confidence.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(low_confidence, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'low_confidence.started')
+                # update status
+                low_confidence.status = STARTED
+                low_confidence.setAutoDraw(True)
+            
+            # if low_confidence is active this frame...
+            if low_confidence.status == STARTED:
+                # update params
+                pass
+            
+            # check for quit (typically the Esc key)
+            if defaultKeyboard.getKeys(keyList=["escape"]):
+                thisExp.status = FINISHED
+            if thisExp.status == FINISHED or endExpNow:
+                endExperiment(thisExp, win=win)
+                return
+            # pause experiment here if requested
+            if thisExp.status == PAUSED:
+                pauseExperiment(
+                    thisExp=thisExp, 
+                    win=win, 
+                    timers=[routineTimer], 
+                    playbackComponents=[]
+                )
+                # skip the frame we paused on
+                continue
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                check_up_routine.forceEnded = routineForceEnded = True
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in check_up_routine.components:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # --- Ending Routine "check_up_routine" ---
+        for thisComponent in check_up_routine.components:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        # store stop times for check_up_routine
+        check_up_routine.tStop = globalClock.getTime(format='float')
+        check_up_routine.tStopRefresh = tThisFlipGlobal
+        thisExp.addData('check_up_routine.stopped', check_up_routine.tStop)
+        # check responses
+        if check_up_resp.keys in ['', [], None]:  # No response was made
+            check_up_resp.keys = None
+            # was no response the correct answer?!
+            if str(certainty_expectation_answer).lower() == 'none':
+               check_up_resp.corr = 1;  # correct non-response
+            else:
+               check_up_resp.corr = 0;  # failed to respond (incorrectly)
+        # store data for conditions_loop (TrialHandler)
+        conditions_loop.addData('check_up_resp.keys',check_up_resp.keys)
+        conditions_loop.addData('check_up_resp.corr', check_up_resp.corr)
+        if check_up_resp.keys != None:  # we had a response
+            conditions_loop.addData('check_up_resp.rt', check_up_resp.rt)
+            conditions_loop.addData('check_up_resp.duration', check_up_resp.duration)
+        # the Routine "check_up_routine" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         
         # --- Prepare to start Routine "pain_fix" ---
